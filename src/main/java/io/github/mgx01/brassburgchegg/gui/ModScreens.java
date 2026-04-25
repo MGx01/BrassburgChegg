e463e80942f4d1706462acb25103507339967c76
MGx01 <2001.Marcel.Graf@gmail.com>
2026-04-25 23:54:34 +0200

    BlockBase Added

--- src/main/java/io/github/mgx01/brassburgchegg/gui/ModScreens.java
+++ src/main/java/io/github/mgx01/brassburgchegg/gui/ModScreens.java
@@ -1,19 +1,17 @@
-package io.github.mgx01.brassburgchegg.gui;
+package io.github.mgx01.brassburgchegg.gui; // Matches your directory structure
+
 import io.github.mgx01.brassburgchegg.menus.ModMenus;
 import io.github.mgx01.brassburgchegg.gui.deck.DeckScreen;
-import net.minecraft.client.gui.screens.MenuScreens;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent; // The New 1.21 Event!
 
-@EventBusSubscriber(modid = "brassburgchegg", value = Dist.CLIENT)
+//@EventBusSubscriber(modid = "brassburgchegg", value = Dist.CLIENT)
 public class ModScreens {
+
     @SubscribeEvent
-    public static void registerScreens(FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            MenuScreens.register(ModMenus.DECK_MENU.get(), DeckScreen::constructScreen);
-            // MenuScreens.register(ModMenus.SCEPTER_MENU.get(), ScepterScreen::constructScreen);
-        });
+    public static void registerScreens(RegisterMenuScreensEvent event) {
+        event.register(ModMenus.DECK_MENU.get(), DeckScreen::new);
     }
 }
--- src/main/java/io/github/mgx01/brassburgchegg/gui/deck/DeckScreen.java
+++ src/main/java/io/github/mgx01/brassburgchegg/gui/deck/DeckScreen.java
@@ -1,4 +1,28 @@
 package io.github.mgx01.brassburgchegg.gui.deck;
 
-public class DeckScreen {
+import io.github.mgx01.brassburgchegg.menus.deck.DeckMenu;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+
+public class DeckScreen extends AbstractContainerScreen<DeckMenu> {
+
+    // THE CONSTRUCTOR
+    public DeckScreen(DeckMenu menu, Inventory playerInventory, Component title) {
+        super(menu, playerInventory, title);
+    }
+
+    // REQUIRED RENDERING METHODS
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
+        this.renderTooltip(guiGraphics, mouseX, mouseY);
+    }
+
+    @Override
+    protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
+        // Custom UI background
+    }
 }
